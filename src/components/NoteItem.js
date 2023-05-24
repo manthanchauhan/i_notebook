18ba877a7d69d20635929a50d7f40da210943e69
manthan <manthanchauhan@indiagold.co>
2023-05-24 20:40:34 +0530

    added fa

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -6,11 +6,15 @@ const NoteItem = (props) => {
     <div className="col-md-3">
       <div className="card my-3">
         <div className="card-body">
-          <h5 className="card-title">{note.title}</h5>
+          <div className="d-flex mb-3">
+            <h5 className="card-title me-auto p-2">{note.title}</h5>
+            <i className="fa-regular fa-trash-can mx-2"></i>
+            <i class="fa-regular fa-pen-to-square mx-2"></i>
+          </div>
           <p className="card-text">{note.description}</p>
         </div>
       </div>
-      </div>
+    </div>
   );
 };
 
